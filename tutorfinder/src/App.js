2e1f2914a1ca3e2b65d84577dc50f26a6aa74e84
jeries <geriesmazzawi@gmail.com>
2020-10-07 23:17:14 +0300

    Routers

--- tutorfinder/src/App.js
+++ tutorfinder/src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './App.css';
 
+// To navigate between pages 
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom';
+
 //components
-import Nav from './Components/Nav/Nav'  
+import Nav from './Components/Nav/Nav' 
+import HomePage from './Components/HomePage/HomePage'
+import SignIn from './Components/SignIn/SignIn'
 
 function App() {
   return (
+    <Router>
     <div className="App"> 
       <Nav/>
-      <h1> tutor Finder </h1>
+      <Switch> 
+
+      <Route path="/" exact component={HomePage} />
+      <Route path="/SignIn" component={SignIn} />
+       
+
+      </Switch>
      
+      
     </div>
+    </Router>
   );
 }
 
--- tutorfinder/src/Components/Nav/Nav.jsx
+++ tutorfinder/src/Components/Nav/Nav.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import './Nav.css';
 
+import {useHistory} from "react-router-dom";
 function Nav (){
-    
+    let History=useHistory();
+
+    function HandleSignIn(e){
+        e.preventDefault();
+        History.push('/SignIn')
+    }
+
     return (
     <div>
         <div className="navigation">
             <ul className="navigation__menu">
                 <li id="navigation__logo"> TutorFinder </li>
-                <li id="navigation__signIn"> Sign IN  </li>
+                <li id="navigation__signIn" onClick={HandleSignIn}> Sign IN  </li>
             </ul>
         </div>
-    </div>)
-    ;
+    </div>
+    );
 
 }
 
