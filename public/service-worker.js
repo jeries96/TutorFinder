574bb2ffb225b4a2b058bf9a9e2a6ccf389733d0
jeries <geriesmazzawi@gmail.com>
2020-10-17 23:34:37 +0300

    ini

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89c243ae4937053ac0d8e5a850058625.js"
+  "/precache-manifest.063f2987dcf07ea570e710e4382921d7.js"
 );
 
 self.addEventListener('message', (event) => {
