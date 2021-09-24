c506a482d57b6b507263275418ca0ea989abb194
Jeries <geriesmazzawi@gmail.com>
2021-09-24 17:40:45 +0300

    fixes/css

--- tutorfinder/src/Pages/HomePage/MainPageProfile.jsx
+++ tutorfinder/src/Pages/HomePage/MainPageProfile.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import image from '../../utils/emek.jpg'
 import './MainPageProfile.css';
 import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Cookies from "js-cookie";
 
 
 function MainPageProfile() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  useEffect(() => {
+    let token = Cookies.get('loginToken');
+
+    if (token != undefined) {
+      setIsLoggedIn(true)
+    }
+  }, [isLoggedIn])
 
 
     return(
@@ -16,9 +27,18 @@ function MainPageProfile() {
          <div className="container position-relative" data-aos="zoom-in" data-aos-delay="100">
         <h1>צברו כישורים, תיהנו, ובנו לכם ידע טוב יותר</h1>
         <h2>להתחברות או להרשמה, תלחצו על הכפתורים  למטה </h2>
-        <Link to='/SignIn'>  <a href="courses.html" className="btn-get-started">התחבר </a> </Link>
-        <Link to='/SignUp'>  <a href="courses.html" className="btn-get-started-right-side">הירשם </a> </Link>
+        
+        
+          { 
+          isLoggedIn === false && 
+            <div>
+            <Link to='/SignIn'>  <a href="courses.html" className="btn-get-started">התחבר </a> </Link>
+            <Link to='/SignUp'>  <a href="courses.html" className="btn-get-started-right-side">הירשם </a> </Link>
+            </div>
+          }
+
         </div>
+        
   </section>
 </div>
 );
